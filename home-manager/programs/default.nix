f07d3e3162de15923647cdcfce9b7d493237b877
mewoocat <azure.horizon.4@gmail.com>
2024-05-18 00:04:28 -0500

    test

--- home-manager/programs/default.nix
+++ home-manager/programs/default.nix
@@ -5,11 +5,13 @@
   ### Programs ###
      programs.git = {
         enable = true;
+        /*
         extraConfig = {
           credential.helper = "${
               pkgs.git.override { withLibsecret = true; }
             }/bin/git-credential-libsecret";
         };
+        */
       };
 
   programs.vscode = {
