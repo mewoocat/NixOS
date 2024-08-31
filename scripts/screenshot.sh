c3937c4033afe83e14edf40fef138238628c51f4
mewoocat <azure.horizon.4@gmail.com>
2024-08-30 22:43:59 -0500

    Prevent multiple satty instances

--- scripts/screenshot.sh
+++ scripts/screenshot.sh
@@ -2,4 +2,5 @@
 
 currentMonitor=$(hyprctl activeworkspace -j | jq --raw-output '.monitor')
 echo $currentMonitor
+pkill satty
 grim -o $currentMonitor - | satty -f - --early-exit --initial-tool crop --copy-command wl-copy --fullscreen
