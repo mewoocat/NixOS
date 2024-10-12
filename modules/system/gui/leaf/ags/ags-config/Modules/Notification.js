f2007d8209aa2982c71a5c98dd07823a903969aa
mewoocat <azure.horizon.4@gmail.com>
2024-10-12 00:47:12 -0500

    Improving notifications

--- modules/system/gui/leaf/ags/ags-config/Modules/Notification.js
+++ modules/system/gui/leaf/ags/ags-config/Modules/Notification.js
@@ -1,17 +1,18 @@
 import Notifications from 'resource:///com/github/Aylur/ags/service/notifications.js';
 import Widget from 'resource:///com/github/Aylur/ags/widget.js';
+import GLib from 'gi://GLib';
 import { lookUpIcon } from 'resource:///com/github/Aylur/ags/utils.js';
 
 // Notification service config
-Notifications.clearDelay = 500 // Helps prevent crashes when calling `Notifications.clear()`
+Notifications.clearDelay = 100 // Helps prevent crashes when calling `Notifications.clear()`
 
 /** @param {import('resource:///com/github/Aylur/ags/service/notifications.js').Notification} n */
 const NotificationIcon = ({ app_entry, app_icon, image }) => {
     if (image) {
         return Widget.Box({
             css: `
-                min-width: 2rem;
-                min-height: 2rem;
+                min-width: 3rem;
+                min-height: 3rem;
                 background-image: url("${image}");
                 background-size: contain;
                 background-repeat: no-repeat;
@@ -21,11 +22,12 @@ const NotificationIcon = ({ app_entry, app_icon, image }) => {
     }
 
     let icon = 'dialog-information-symbolic';
-    if (lookUpIcon(app_icon))
-        icon = app_icon;
-
     if (app_entry && lookUpIcon(app_entry))
         icon = app_entry;
+    else if (lookUpIcon(app_icon))
+        icon = app_icon;
+
+    print("INFO: Notification icon: " + icon)
 
     return Widget.Icon({
         size: 48, 
@@ -37,13 +39,34 @@ const NotificationIcon = ({ app_entry, app_icon, image }) => {
 export const Notification = n => {
     const icon = Widget.Box({
         vpack: 'center',
-        vexpand: true,
         class_name: 'icon',
         child: NotificationIcon(n),
     });
 
-    const title = Widget.Label({
-        class_name: 'title',
+    const appName = Widget.Label({
+        class_name: 'sub-text',
+        xalign: 0,
+        justification: 'left',
+        hexpand: true,
+        max_width_chars: 24,
+        truncate: 'end',
+        wrap: true,
+        label: n.app_name,
+        use_markup: true,
+    });
+
+    const time = Widget.Label({
+        class_name: 'sub-text',
+        xalign: 1, // Move text to end of container
+        justification: 'right',
+        hexpand: true,
+        max_width_chars: 24,
+        truncate: 'end',
+        label: GLib.DateTime.new_from_unix_local(n.time).format("%l:%M %P"),
+    });
+
+    const summary = Widget.Label({
+        class_name: 'medium-text',
         xalign: 0,
         justification: 'left',
         hexpand: true,
@@ -55,7 +78,7 @@ export const Notification = n => {
     });
 
     const body = Widget.Label({
-        class_name: 'body',
+        class_name: 'small-text',
         hexpand: true,
         use_markup: true,
         xalign: 0,
@@ -80,20 +103,28 @@ export const Notification = n => {
         child: Widget.Box({
             class_name: `${n.urgency} container`,
             vertical: true,
+            vpack: "center",
+            vexpand: true,
             css: `
-                margin-right: 0.8em;
-                margin-bottom: 0.8em;
                 min-width: 14em;
                 min-height: 4em;
             `,
             children: [
                 Widget.Box({
+                    spacing: 8,
                     children: [
                         icon,
                         Widget.Box({
                             vertical: true,
                             children: [
-                                title,
+                                Widget.Box({
+                                    homogeneous: true,
+                                    children: [
+                                        appName,
+                                        time,
+                                    ]
+                                }),
+                                summary,
                                 body,
                             ],
                         }),
