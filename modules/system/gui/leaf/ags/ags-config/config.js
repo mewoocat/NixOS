7caefe9111bc9ac694d19c50cba28b94aa6b6073
mewoocat <azure.horizon.4@gmail.com>
2024-10-13 11:13:30 -0500

    Refractoring imports

--- modules/system/gui/leaf/ags/ags-config/config.js
+++ modules/system/gui/leaf/ags/ags-config/config.js
@@ -1,5 +1,6 @@
 import App from 'resource:///com/github/Aylur/ags/app.js';
 import Hyprland from 'resource:///com/github/Aylur/ags/service/hyprland.js';
+import Utils from 'resource:///com/github/Aylur/ags/utils.js';
 // Add icons in assets to icon set
 //Gtk.IconTheme.get_default().append_search_path(`${App.configDir}/assets`);
 // Need to do this before importing anything else since importing could try to eval an custom icon which has not been added yet
@@ -11,31 +12,31 @@ import {data, GetOptions, Options, LoadOptionWidgets} from './Options/options.js
 // Loads json config and options
 GetOptions()
 
-import { generalSettings, displaySettings } from './Modules/Settings.js';
+import * as Common from './Common.js';
+import * as Settings from './Modules/Settings.js';
 // Load the user options as widgets into the generalSettings FlowBox
-LoadOptionWidgets(Options.user.general, generalSettings)
-LoadOptionWidgets(Options.user.display, displaySettings)
+LoadOptionWidgets(Options.user.general, Settings.generalSettings)
+LoadOptionWidgets(Options.user.display, Settings.displaySettings)
 
-import { exec } from 'resource:///com/github/Aylur/ags/utils.js'
-import { forMonitors } from './Common.js';
-import { monitorFile } from 'resource:///com/github/Aylur/ags/utils.js';
-import { ActivityCenter } from './Windows/ActivityCenter.js';
-import { NotificationPopup } from './Windows/NotificationPopups.js';
-import { Dock } from './Windows/Dock.js';
-import { Launcher } from './Windows/Launcher.js';
-import { Bar } from './Windows/Bar.js';
-import { ControlPanel } from './Windows/ControlPanel.js';
-import { monitors } from './Monitors.js'
+
+// Import windows
+import * as ActivityCenter from './Windows/ActivityCenter.js';
+import * as NotificationPopup from './Windows/NotificationPopups.js';
+import * as Dock from './Windows/Dock.js';
+import * as Launcher from './Windows/Launcher.js';
+import * as Bar from './Windows/Bar.js';
+import * as ControlPanel from './Windows/ControlPanel.js';
+import * as Monitors from './Monitors.js'
 
 print("importing settings")
-import { Settings } from './Windows/Settings.js';
-import { GenerateCSS, css } from './Style/style.js'
+import * as SettingsWin from './Windows/Settings.js';
+import * as Style from './Style/style.js'
 
 // Compile and apply scss as css
-GenerateCSS()
+Style.GenerateCSS()
 
 // Monitor for color changes and reapply
-monitorFile(`${App.configDir}/Style/_colors.scss`, GenerateCSS);
+Utils.monitorFile(`${App.configDir}/Style/_colors.scss`, Style.GenerateCSS);
 
 function InitilizeWindows(){
 
@@ -43,29 +44,29 @@ function InitilizeWindows(){
     let defaultMonitorID = 0
 
     // Find the id num associated with the monitor identifier
-    if (monitors[userDefaultMonitor] != undefined){
-        defaultMonitorID = monitors[userDefaultMonitor]
+    if (Monitors.monitors[userDefaultMonitor] != undefined){
+        defaultMonitorID = Monitors.monitors[userDefaultMonitor]
     }
 
     print("INFO: defaultMonitorID: " + defaultMonitorID)
 
     const windows = [
-        Launcher(), 
         // What does ... do? Spread syntax allows you to deconstruct an array or object into separate variables.
         // ... here returns the array output of forMonitors as a individual elements so they are not nested in the parrent array
-        //...forMonitors(Bar), 
-        Bar(defaultMonitorID),
-        ControlPanel(),
-        ActivityCenter(),
-        NotificationPopup(), 
-        Settings(),
-        //Dock()
+        //...Common.forMonitors(Bar), 
+        Bar.Bar(defaultMonitorID),
+        Launcher.Launcher(), 
+        ControlPanel.ControlPanel(),
+        ActivityCenter.ActivityCenter(),
+        NotificationPopup.NotificationPopup(), 
+        SettingsWin.Settings(),
+        //Dock.Dock()
     ]
     return windows
 }
 
 App.config({
-    style: css, 
+    style: Style.css, 
     closeWindowDelay: {
         "ControlPanel":     150, // milliseconds
         "applauncher":      150, // milliseconds
