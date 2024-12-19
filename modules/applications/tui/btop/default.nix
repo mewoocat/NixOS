744021215b8dc89a584635b29158739a8215bfd6
mewoocat <azure.horizon.4@gmail.com>
2024-12-18 19:48:29 -0600

    Symlinking btop.conf

--- modules/applications/tui/btop/default.nix
+++ modules/applications/tui/btop/default.nix
@@ -8,8 +8,17 @@
     btop
   ];
 
-  systemd.user.tmpfiles.users.eXia.rules = [
+  home-manager.users.${config.username} = {
+    systemd.user.tmpfiles.rules = [
+      "L /home/${config.username}/.config/btop/btop.conf - - - - ${./btop.conf}"
+    ];
+  };
+
+  # This has issues
+  /*
+  systemd.user.tmpfiles.users.${config.username}.rules = [
     "L+ /home/${config.username}/.config/btop/btop.conf - - - - ${./btop.conf}"
   ];
+  */
 
 }
