071b24e95e4eb7a3d23b7330c7e8ad6ff2f7ab22
mewoocat <azure.horizon.4@gmail.com>
2025-01-23 07:41:59 -0600

    Opening DNS port

--- modules/servers/ad-guard-home/default.nix
+++ modules/servers/ad-guard-home/default.nix
@@ -1,4 +1,12 @@
 {config, pkgs, ...}: {
+
+  # Open DNS port
+  networking.firewall = {
+    enable = true;
+    allowedTCPPorts = [ 53 ];
+    allowedUDPPorts = [ 53 ];
+  };
+
   services.adguardhome = {
     enable = true;
     port = 3000; # Port to serve HTTP pages on
