3287e6ee8459205c5df322f9e90617e6ceae0520
mewoocat <azure.horizon.4@gmail.com>
2025-04-12 20:04:18 -0500

    hyprland plugin init workaround

--- modules/system/desktop-environments/leaf/hyprland/default.nix
+++ modules/system/desktop-environments/leaf/hyprland/default.nix
@@ -15,9 +15,15 @@
   */
 
   hjem.users.${config.username}.files = {
-    ".config/hyprland/hyprland.conf" = {
+    ".config/hypr/hyprland.conf" = {
       clobber = true;
       source = ./hyprland.conf;
+    };
+    ".config/hypr/nixManaged.conf" = {
+      clobber = true;
+      text = ''
+        plugin = ${inputs.hyprland-plugins.packages.${pkgs.system}.hyprbars}/lib/libhyprbars.so
+      '';
     };
   };
 
@@ -26,6 +32,9 @@
     package = inputs.hyprland.packages."${pkgs.system}".hyprland;
     portalPackage = inputs.hyprland.packages."${pkgs.system}".xdg-desktop-portal-hyprland;
     withUWSM = true;
+    # This adds "plugin = ..." to /etc/xdg/hypr/hyprland.conf
+    # However having the plugin value set here doesn't seem to work
+    # Manully adding it to the main config file above
     plugins = [
       inputs.hyprland-plugins.packages.${pkgs.system}.hyprbars
     ];
