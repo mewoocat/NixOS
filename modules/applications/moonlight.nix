6eb6652f3b2a3ce3b65ac7a000e3f6678abe728a
mewoocat <azure.horizon.4@gmail.com>
2025-10-29 16:48:32 -0500

    moonlight/sunshine cleanup and disabling

--- modules/applications/moonlight.nix
+++ modules/applications/moonlight.nix
@@ -6,29 +6,9 @@
   ...
 }: {
   # Moonlight / Sunshine
-  ######################################################################
-  /*
-  users.users.${config.username}.packages = with pkgs; [
-    (sunshine.override { cudaSupport = true; })
-  ];
-  */
   services.sunshine = {
-    enable = true;
+    enable = false;
     capSysAdmin = true; # only needed for Wayland -- omit this when using with Xorg
     openFirewall = true;
   };
-  /*
-  services.avahi.enable = true;
-  services.avahi.publish.userServices = true;
-  networking.firewall = {
-    enable = true;
-    allowedTCPPorts = [47984 47989 47990 48010];
-    allowedUDPPortRanges = [
-      {
-        from = 47998;
-        to = 48000;
-      }
-    ];
-  };
-  */
 }
