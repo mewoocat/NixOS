3eb7fefb65432d7f3982fa14af9a7e721bf288a0
mewoocat <azure.horizon.4@gmail.com>
2025-12-07 22:51:48 -0600

    import fix

--- hosts/default.nix
+++ hosts/default.nix
@@ -3,6 +3,7 @@
 {
   scythe = import ./scythe {inputs = inputs;};
   obsidian = import ./obsidian {inputs = inputs;};
+  orchid = import ./orchid {inputs = inputs;};
   maple = import ./maple {inputs = inputs;};
   chrysanthemum = import ./chrysanthemum {inputs = inputs;};
 }
