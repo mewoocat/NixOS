3ad1083a23a411d9650992c75ad0b4fc9455aaf2
mewoocat <azure.horizon.4@gmail.com>
2025-12-23 06:53:09 -0600

    mpv and pix

--- modules/system/desktop-environments/leaf/core-applications/default.nix
+++ modules/system/desktop-environments/leaf/core-applications/default.nix
@@ -10,25 +10,53 @@
 
   users.users.${config.username} = {
     packages = with pkgs; [
-      gnome-calculator
-      eog
-      gthumb
-      gnome-text-editor
-      #nemo-with-extensions
 
+      # File Manager
+      pcmanfm-qt 
       kdePackages.dolphin
       kdePackages.qtsvg # Needed for icons to work in dolphin
+      #nemo-with-extensions
 
       # Calendar
       #libsForQt5.merkuro
       #kdePackages.merkuro
 
+      # Disks
       kdePackages.partitionmanager
+
+      # Calculator
+      gnome-calculator
       kdePackages.kcalc
 
-      pcmanfm-qt 
+      # Media viewer
+      (mpv.override {
+        scripts = with pkgs.mpvScripts; [
+          thumbfast
+          mpv-image-viewer.image-positioning # Need to setup keybinds
+          mpv-image-viewer.minimap
+          mpv-image-viewer.status-line
+          mpv-image-viewer.freeze-window
+        ];
+      })
+      pix
+      #eog
+      #gthumb
 
-      mpv # For video playback
+      # Text editor
+      gnome-text-editor
     ];
   };
+
+  # TODO: Look into https://github.com/occivink/mpv-image-viewer/blob/master/mpv.conf for image related conf
+  # TODO: Look into https://github.com/occivink/mpv-image-viewer/blob/master/input.conf#L19-L67 for image-positioning keybinds
+  hjem.users.${config.username}.files = {  
+    ".config/mpv/mpv.conf" = {
+      clobber = true;
+      text = ''
+        keep-open
+        idle=yes
+      '';
+    };
+  };
+
 }
