e40bccc1c553f9fd4ce48f5ca805e74a7cc782f7
mewoocat <azure.horizon.4@gmail.com>
2026-02-12 16:55:40 -0600

    theme stuff

--- modules/system/desktop-environments/leaf/theming/default.nix
+++ modules/system/desktop-environments/leaf/theming/default.nix
@@ -15,20 +15,33 @@
   #wallust = pkgs.callPackage ./wallust.nix {};
 
   # Default GTK theme
-  /*
-  gtk-theme = pkgs.adw-gtk3.overrideAttrs {
+  adw-gtk3-override = let 
+      colorsImportText = "@import \"../../../../../.config/gtk-3.0/colors.css\";";
+      lineToInsertAfter = "125";
+  in pkgs.adw-gtk3.overrideAttrs {
     postInstall = ''
       # MODIFICATION: Use color variables from leaf
-      echo "@import "../../../../../.config/leaf-de/theme/gtk.css"" >> $out/share/themes/adw-gtk3-dark/gtk-3.0/gtk.css
+      #echo "@import \"../../../../../.config/leaf-de/theme/gtk.css\"" >> $out/share/themes/adw-gtk3-dark/gtk-3.0/gtk.css
+
+      # Modify the gtk css to override specified colors
+      # This is needed since ~/.config/gtk-3.0/gtk.css can't be hot reloaded.  If we want to dynamically change
+      # the colors of the theme without requiring a restart of any running apps, the theme itself will need to
+      # import the dynamic colors
+      sed -i '${lineToInsertAfter}a ${colorsImportText}' $out/share/themes/adw-gtk3-dark/gtk-3.0/gtk.css
+      sed -i '${lineToInsertAfter}a ${colorsImportText}' $out/share/themes/adw-gtk3-dark/gtk-3.0/gtk-dark.css
+
+      sed -i '${lineToInsertAfter}a ${colorsImportText}' $out/share/themes/adw-gtk3/gtk-3.0/gtk.css
+      # Not sure if the gtk-dark.css needs to be modified.  Seems it is different from gtk.css, unlike in adw-gtk3-dark.
     '';
   };
-  */
+  /*
   adw-gtk3-leaf = pkgs.fetchFromGitHub {
     owner = "mewoocat";
     repo = "adw-gtk3-leaf";
     rev = "443594813831c00a7ba59f45a8442ebe9547a69d";
     sha256 = "sha256-/9TKfZiyzcaaT+jb+Tk+mrlYYUlh+kqDpPVDCQoH+R8=";
   };
+  */
 
 in {
 
@@ -90,7 +103,8 @@ in {
     inputs.matugen.packages.x86_64-linux.default
 
     # Gtk
-    adw-gtk3 # Main GTK theme
+    #adw-gtk3 # Main GTK theme
+    adw-gtk3-override
     whitesur-gtk-theme
     orchis-theme
     shades-of-gray-theme
@@ -117,12 +131,20 @@ in {
         source = ./matugen;
       };
 
+      # Note that this file is not dynamically loaded by gtk3 apps
+      # Apps have to be restarted for any changes here to take effect
+      #
+      # So the only way I see for hot reloading the colors on a gtk theme is to modify
+      # the theme itself to import it colors dynamically.  This seems to get reloaded
+      # on the fly when reapplying the theme.
+      /*
       ".config/gtk-3.0/gtk.css" = {
         # This imports the colors generated from matugen
         text = ''
           @import 'colors.css';
         '';
       };
+      */
 
       ".config/gtk-4.0/gtk.css" = {
         # This imports the colors generated from matugen
