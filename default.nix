a3134c9522c4840ac012eab859bcf89c9ab01f30
mewoocat <azure.horizon.4@gmail.com>
2026-06-06 21:31:17 -0400

    flake-inputs func

--- default.nix
+++ default.nix
@@ -1,19 +1,27 @@
+# TODO
+# - look into tack
+
 let
   sources = import ./npins;
 
   # Flake compatibility
-  getFlakeOutputs = npinsSource: (import sources.flake-compat { src = npinsSource; }).outputs;
+  #getFlakeOutputs = npinsSource: (import sources.flake-compat { src = npinsSource; }).outputs;
+  getFlakeOutputs = {npinsSource, followsNixpkgs ? false}: ((import sources.flake-inputs).import-flake {
+    src = npinsSource;
+    overrides = if followsNixpkgs then { nixpkgs = sources.nixpkgs.outPath; } else {};
+  });
   inputs = {
-    hjem = getFlakeOutputs sources.hjem;
-    matugen = getFlakeOutputs sources.matugen;
+    hjem = getFlakeOutputs { npinsSource = sources.hjem; };
+    matugen = getFlakeOutputs { npinsSource = sources.matugen; };
     #quickshell = getFlakeOutputs sources.quickshell; # borked
     #niri = getFlakeOutputs sources.niri;
     #qtengine = getFlakeOutputs sources.qtengine;
 
     # This uses flake-inputs instead of flake-compat, in order to override the nixpkgs dependency.  qtengine requires this or else issues can occur due to mismatches
-    qtengine =  (import sources.flake-inputs).import-flake { src = sources.qtengine; overrides = { nixpkgs = sources.nixpkgs.outPath; }; };
-    nix-gaming = getFlakeOutputs sources.nix-gaming;
-    nvim-nvf = getFlakeOutputs sources.nvim-nvf;
+    #qtengine =  (import sources.flake-inputs).import-flake { src = sources.qtengine; overrides = { nixpkgs = sources.nixpkgs.outPath; }; };
+    qtengine = getFlakeOutputs { npinsSource = sources.qtengine; followsNixpkgs = true; };
+    nix-gaming = getFlakeOutputs { npinsSource = sources.nix-gaming; };
+    nvim-nvf = getFlakeOutputs { npinsSource = sources.nvim-nvf; };
   };
 
   # System builder
