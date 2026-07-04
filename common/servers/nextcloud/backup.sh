508e220608952add4b79b2cb285c2c6a0b70a7d7
mewoocat <azure.horizon.4@gmail.com>
2026-07-04 12:07:42 -0400

    backup script update

--- common/servers/nextcloud/backup.sh
+++ common/servers/nextcloud/backup.sh
@@ -4,7 +4,7 @@ hostName="maple"
 
 # TODO: use nextcloud use for permissions
 rsync -vvv --dry-run -Aavx \
-  "$hostName:/home/eXia/test" "/tmp/test"
+  --rsync-path "sudo rsync" \
+  "$hostName:/var/lib/nextcloud" "/run/media/eXia/server-backup/nextcloud-backup_$(date +"%Y%m%d")/"
+  #"$hostName:/home/eXia/test" "/tmp/test"
   #-e "ssh -tt" \
-  #--rsync-path "rsync" \
-  #"$hostName:/var/lib/nextcloud" "/run/media/eXia/server-backup/nextcloud-backup_$(date +"%Y%m%d")/"
